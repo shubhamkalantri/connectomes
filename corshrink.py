51686c54419b645bc9dfd57a9fbcfe99578473d8
Shubham Kalantri <shubhamkalantri123@gmail.com>
2025-03-24 14:35:03 +0000

    fix corshrink

--- corshrink.py
+++ corshrink.py
@@ -5,13 +5,18 @@ def corshrink(data: np.ndarray, lamb: float) -> np.ndarray:
     n = data.shape[0]
     p = data.shape[1]
 
-    w = np.ones((n, 1)) * 1 / n
+    data = data - np.mean(data, axis=0)
+    data = data / np.std(data, axis=0, ddof=1)
 
-    h1 = 1 / (1 - w.T @ w)
+    r = np.corrcoef(data, rowvar=False)
 
-    r0 = h1 * (data * np.sqrt(w)).T @ data * np.sqrt(w)
+    vr = np.zeros((p, p))
+    for i in range(p):
+        for j in range(p):
+            if i != j:
+                vr[i, j] = (1 / (n - 1)) * ((1 - r[i, j] ** 2) ** 2)
 
-    power = (1 - lamb) * r0
-    np.fill_diagonal(power, 1)
+    Rhat = (1 - lamb) * r
+    np.fill_diagonal(Rhat, 1)
 
-    return power
+    return Rhat
